690e1bd105307c805e7ac86333005c99750205a4
Henrique Lopes <oprometeumoderno@gmail.com>
2018-04-19 22:48:46 -0300

    cut off some redundant divisions in problem 24

--- 024.rb
+++ 024.rb
@@ -2,15 +2,15 @@ TARGET = 1000000 - 1
 
 def nth_permutation(digits, target)
   return digits[0] if digits.size == 1
-  possibilities = (1..digits.size).to_a.inject(:*)
+  possibilities = (1...digits.size).to_a.inject(:*)
 
   i = 0
   loop do
-    break if target >= (possibilities/digits.size) * i && target < (possibilities/digits.size) * (i+1)
+    break if target >= (possibilities) * i && target < (possibilities) * (i+1)
     i += 1
   end
 
-  target = target % ((possibilities/digits.size) * i)
+  target = target % ((possibilities) * i)
   return digits.slice!(i) + nth_permutation(digits, target)
 end
 
