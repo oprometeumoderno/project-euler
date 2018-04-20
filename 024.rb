b5ea6f6b12c66a721f3e9861b393aa55421a3333
Henrique Lopes <oprometeumoderno@gmail.com>
2018-04-19 23:15:14 -0300

    makes code for problem 24 even smaller

--- 024.rb
+++ 024.rb
@@ -4,11 +4,7 @@ def nth_permutation(digits, target)
   return digits[0] if digits.size == 1
   possibilities = (1...digits.size).to_a.inject(:*)
 
-  i = 0
-  loop do
-    break if target >= (possibilities) * i && target < (possibilities) * (i+1)
-    i += 1
-  end
+  i = target / possibilities
 
   target = target % ((possibilities) * i)
   return digits.slice!(i) + nth_permutation(digits, target)
