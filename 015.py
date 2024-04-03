856307f0ce40a3bd5ab33a9ba192cc67b8124e68
Henrique de Paula Lopes <oprometeumoderno@gmail.com>
2024-04-03 14:02:02 -0300

    add explanation to 015

--- 015.py
+++ 015.py
@@ -1,6 +1,13 @@
 import functools
 import operator
 
+# simple permutation with repetition
+# to go from corner to corner it requires 20 moves right and 20 moves
+# down, with can be permutated with 20 repetitions for each
+# = 40!/(20! * 20!)
+# = (40 * 39 * 38 * ... * 21) / 20!
+# this could be further reduced using divisions in both sides
+
 numerator = functools.reduce(operator.mul, list(range(21, 41)))
 denominator = functools.reduce(operator.mul, list(range(1,21)))
 
