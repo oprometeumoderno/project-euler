b192965244b448a411be1f0befc0df35c5689d9e
Henrique de Paula Lopes <oprometeumoderno@gmail.com>
2024-04-04 16:17:43 -0300

    add problems 36, 37

---
+++ 037.py
@@ -0,0 +1,33 @@
+from prime import PrimeIterator
+
+
+def get_all_truncated_primes(p):
+    truncated_primes = set([p])
+    truncated_primes.update([int(str(p)[i:]) for i in range(1, len(str(p)))])
+    truncated_primes.update([int(str(p)[:-i]) for i in range(1, len(str(p)))])
+
+    return truncated_primes
+
+
+p = PrimeIterator()
+current_prime = next(p)
+primes = set()
+while current_prime < 10:
+    primes.add(current_prime)
+    current_prime = next(p)
+
+result = 0
+found = 0
+while found < 11:
+    current_prime = next(p)
+    primes.add(current_prime)
+    if (
+        not set([c for c in "02468"]).intersection([c for c in str(current_prime)[1:]])
+        and str(current_prime)[0] not in ["1", "4", "6", "8"]
+        and str(current_prime)[-1] != "1"
+    ):
+        if all([x in primes for x in get_all_truncated_primes(current_prime)]):
+            found += 1
+            result += current_prime
+
+print(result)
