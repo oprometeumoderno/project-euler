182aeff0e16ebe8169083d482d0a1b1510d9ec53
Henrique de Paula Lopes <oprometeumoderno@gmail.com>
2024-04-05 20:44:33 -0300

    add problem 040

---
+++ 040.py
@@ -0,0 +1,18 @@
+sizes = [10**x for x in range(2, 7)]
+
+result = 1
+pos = 10
+i = 10
+
+
+while sizes:
+    if pos + len(str(i)) > sizes[0]:
+        print(sizes[0])
+        d = int(str(i)[sizes[0] - pos - len(str(i))])
+        print(i)
+        print(d)
+        result *= d
+        sizes = sizes[1:]
+    pos += len(str(i))
+    i += 1
+print(result)
