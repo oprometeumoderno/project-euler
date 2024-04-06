2be02b5571ce8f15b443617e559619a43cccf7b2
Henrique de Paula Lopes <oprometeumoderno@gmail.com>
2024-04-06 08:36:36 -0300

    add problem 044

---
+++ 044.py
@@ -0,0 +1,16 @@
+def get_pentagon_number(n):
+    return int((n * (3 * n - 1)) / 2)
+
+
+pentagon_numbers = set()
+i = 1
+found = False
+while not found:
+    pentagon = get_pentagon_number(i)
+    for p_k in pentagon_numbers:
+        p_j = pentagon - p_k
+        if p_j in pentagon_numbers and p_k - p_j in pentagon_numbers:
+            print(f"{p_k - p_j}")
+            break
+    pentagon_numbers.add(pentagon)
+    i += 1
