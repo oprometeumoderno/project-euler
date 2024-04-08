bb182fbf52fbd145a5077d4c384b7cd3240b79e5
Henrique de Paula Lopes <oprometeumoderno@gmail.com>
2024-04-08 15:13:36 -0300

    make sieve faster

--- 010.py
+++ 010.py
@@ -2,16 +2,17 @@ from prime import PrimeIterator
 
 SIZE = 2000000
 
-
+# TODO: move sieve to module
 def solution010():
     sieve = [True] * SIZE
     sieve[0] = False
 
     for n in range(2, SIZE // 2 + 1):
-        m = 2
-        while (n * m) <= len(sieve):
-            sieve[m * n - 1] = False
-            m += 1
+        if sieve[n - 1]:
+            m = 2
+            while (n * m) <= len(sieve):
+                sieve[m * n - 1] = False
+                m += 1
 
     result = 0
     for i in range(len(sieve)):
