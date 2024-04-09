abf736f4cf1bf2f9c0043c5770e5b650e5106a40
Henrique de Paula Lopes <oprometeumoderno@gmail.com>
2024-04-09 00:53:20 -0300

    add test

--- test.py
+++ test.py
@@ -112,6 +112,10 @@ class SolutionTest(unittest.TestCase):
         problem_id = "038"
         self.assertEqual(self.answers[problem_id], self.get_answer(problem_id))
 
+    def testProblem047(self):
+        problem_id = "047"
+        self.assertEqual(self.answers[problem_id], self.get_answer(problem_id))
+
     def testProblem049(self):
         problem_id = "049"
         self.assertEqual(self.answers[problem_id], self.get_answer(problem_id))
