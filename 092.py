096744f426fb5e0091d874b24aa5c2ec9a1c51fb
Henrique de Paula Lopes <oprometeumoderno@gmail.com>
2024-04-09 01:59:24 -0300

    makes 092 faster

--- 092.py
+++ 092.py
@@ -3,19 +3,21 @@ from itertools import permutations
 CHAIN_CACHE = {1: False, 89: True}
 
 
-def is_89(n):
+def is_89_cache(n):
     if n in CHAIN_CACHE:
         return CHAIN_CACHE[n]
     else:
         new_n = sum([(int(d) ** 2) for d in str(n)])
-        CHAIN_CACHE[n] = is_89(new_n)
+        CHAIN_CACHE[n] = is_89_cache(new_n)
         return CHAIN_CACHE[n]
 
 
 def solution092():
     result = 0
-    for i in range(1, 10000000):
-        if is_89(i):
+    for i in range(1, 568):
+        if is_89_cache(i):
             result += 1
-
+    for i in range(568, 10000000):
+        if CHAIN_CACHE[sum([int(c) ** 2 for c in str(i)])]:
+            result += 1
     return result
