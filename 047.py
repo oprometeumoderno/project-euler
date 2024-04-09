8d2531f3fc4a6a581063f00ea1fb620fe3014f8b
Henrique de Paula Lopes <oprometeumoderno@gmail.com>
2024-04-09 00:52:28 -0300

    refactor 047, add test

--- 047.py
+++ 047.py
@@ -1,36 +1,27 @@
-def get_prime_factors(n, primes):
-    current_prime = 0
-    i = n
-    factors = {}
-    while current_prime < len(primes) and i > 1:
-        p = primes[current_prime]
-        if i % p == 0:
-            i //= p
-            if p not in factors:
-                factors[p] = 1
-            else:
-                factors[p] += 1
-        else:
-            current_prime += 1
-    if current_prime == len(primes):
-        primes.append(n)
-        return [(n, 1)]
-    else:
-        return [(factor, factors[factor]) for factor in factors]
+import math
+
+
+def get_prime_factors(n, factors):
+    i = 0
+    while i < math.isqrt(n):
+        if n % (i + 2) == 0:
+            return factors[i].union(factors[n // (i + 2) - 2])
+        i += 1
+    return {n}
 
 
 def solution047():
-    primes = [2]
+    factors = [{2}, {3}]
     distinct_numbers = []
     target = 4
-    i = 3
+    i = 4
 
     while len(distinct_numbers) < target:
-        factors = get_prime_factors(i, primes)
-        if len(factors) == target:
+        distinct_factors = get_prime_factors(i, factors)
+        factors.append(distinct_factors)
+        if len(distinct_factors) == target:
             distinct_numbers.append(i)
         else:
             distinct_numbers = []
         i += 1
-
     return distinct_numbers[0]
